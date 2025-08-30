9d0208a7e6efe59a7b48cf3f3cb786b778256b8d
kumar gaurav <kgaurav@longboatgroupllc.com>
2025-08-30 19:31:31 +0530

    updated code

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
-#app.py
 import streamlit as st
 import pandas as pd
 import joblib
 from preprocessing import preprocess_inference
+from inference import predict_transaction
 
 MODEL_PATH = "artifacts/model.pkl"
-model = joblib.load(MODEL_PATH)
 
 st.title("Fraud Detection App (S3-powered)")
 st.write("Upload a CSV file with transactions to predict fraud.")
 
 uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
 if uploaded_file is not None:
+    # Read CSV normally
     df = pd.read_csv(uploaded_file)
     st.write("Uploaded Data", df)
 
     try:
-        X = preprocess_inference(df)
-        preds = model.predict(X)
-        probs = model.predict_proba(X)[:, 1]
+        # Pass the DataFrame directly to inference
+        preds, probs = predict_transaction(df)
 
         results = df.copy()
         results["Prediction"] = preds
         results["Fraud_Prob"] = probs
         st.write("Prediction Results", results)
+
     except Exception as e:
         st.error(f"Error during prediction: {e}")
